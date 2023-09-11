38dd5f3411f2edba019f7fb2150e22a6a56d6113
Kbomi16 <kimbomi2172@naver.com>
2023-09-11 14:58:58 +0900

    basic setting

---
+++ react-todos/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import CSSBaseline from '@mui/material/CssBaseline'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <CSSBaseline/>
+      <h1>Todos</h1>
+    </>
+  )
+}
+
+export default App
+
